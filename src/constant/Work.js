ffd3a2d9185142e3262002818f6cb0534d4afc87
JradOnlyFoods <jared@onlyfoods.ai>
2024-09-27 04:31:46 +0800

    Updated resume

--- src/constant/Work.js
+++ src/constant/Work.js
@@ -1,44 +1,56 @@
 export const Works = [
-   {
-      companyName: "GameMyBiz",
-      workTitle: "Frontend Developer/Engineer",
-      workPlace: "Boise, Idaho, USA",
-      startDate: "Oct 2021",
-      endDate: "Jan 2024",
-      workDescription: [
-         "Mastered using Figma design files to implement pixel-perfect UI/UX designs into web applications",
-         "Enhanced in modern JavaScript (ES6+), including promises and async/await",
-         "Demonstrated strong problem-solving skills, with a track record of debugging and troubleshooting complex frontend issues",
-         "Experienced in version control systems like Git, enabling collaboration and code management within development teams",
-         "Utilized responsive web design principles, ensuring seamless user experiences across various devices and screen sizes",
-         "Implemented with modern CSS methodologies, including CSS-in-JS libraries like styled-components or Emotion"
-      ],
-   },
-   {
-      companyName: "GameMyBiz",
-      workTitle: "Scrum Lead",
-      workPlace: "Boise, Idaho, USA",
-      startDate: "May 2023",
-      endDate: "Nov 2023",
-      workDescription: [
-         "Conducted meetings with the team on Mondays and Fridays (CEO, Operations Director, lead developer, lead marketer, and other team members)",
-         "Sent reminders to each team to make sure they send their updated tasks on Tue, Wed, and Thurs",
-         "Managed tickets of each dev so that each task/ticket is up to date"
-      ]
-   },
-   {
-      companyName: "GameMyBiz",
-      workTitle: "Sprint Lead",
-      workPlace: "Boise, Idaho, USA",
-      startDate: "Oct 2022",
-      endDate: "Apr 2023",
-      workDescription: [
-         "Managed tickets on JIRA (Actual time logged for each dev and their estimations)",
-         "Productivity level went up 30%",
-         "Led meetings on MWF to present our tasks",
-         "Conducted sprint planning and tickets assignment to our dev team",
-         "Gave an overview of sprints each week to the CEO to reach weekly milestones on development",
-         "Ran update of dev tasks to CEO every Tuesday and Thursday"
-      ]
-   },
-]
+  {
+    companyName: "Kynection",
+    workTitle: "Support Developer Engineer",
+    workPlace: "Seaford, Victoria, Australia",
+    startDate: "Apr 2024",
+    endDate: " Aug 2024",
+    workDescription: [
+      "Diagnosed app issues and fixed bugs (scripting code) or performance issues in web applications, particularly those related to JavaScript, PHP, jQuery interactions, and SQL database queries for clients",
+      "Collaborated with developers to apply changes to new features and enhancements, ensuring seamless integration with existing systems for clients",
+      "Provided technical support to users by addressing and resolving their issues, documenting solutions, and offering guidance on best practices",
+    ],
+  },
+  {
+    companyName: "GameMyBiz",
+    workTitle: "Frontend Developer/Engineer",
+    workPlace: "Boise, Idaho, USA",
+    startDate: "Oct 2021",
+    endDate: "Jan 2024",
+    workDescription: [
+      "Mastered using Figma design files to implement pixel-perfect UI/UX designs into web applications",
+      "Enhanced in modern JavaScript (ES6+), including promises and async/await",
+      "Demonstrated strong problem-solving skills, with a track record of debugging and troubleshooting complex frontend issues",
+      "Experienced in version control systems like Git, enabling collaboration and code management within development teams",
+      "Utilized responsive web design principles, ensuring seamless user experiences across various devices and screen sizes",
+      "Implemented with modern CSS methodologies, including CSS-in-JS libraries like styled-components or Emotion",
+    ],
+  },
+  {
+    companyName: "GameMyBiz",
+    workTitle: "Scrum Lead",
+    workPlace: "Boise, Idaho, USA",
+    startDate: "May 2023",
+    endDate: "Nov 2023",
+    workDescription: [
+      "Conducted meetings with the team on Mondays and Fridays (CEO, Operations Director, lead developer, lead marketer, and other team members)",
+      "Sent reminders to each team to make sure they send their updated tasks on Tue, Wed, and Thurs",
+      "Managed tickets of each dev so that each task/ticket is up to date",
+    ],
+  },
+  {
+    companyName: "GameMyBiz",
+    workTitle: "Sprint Lead",
+    workPlace: "Boise, Idaho, USA",
+    startDate: "Oct 2022",
+    endDate: "Apr 2023",
+    workDescription: [
+      "Managed tickets on JIRA (Actual time logged for each dev and their estimations)",
+      "Productivity level went up 30%",
+      "Led meetings on MWF to present our tasks",
+      "Conducted sprint planning and tickets assignment to our dev team",
+      "Gave an overview of sprints each week to the CEO to reach weekly milestones on development",
+      "Ran update of dev tasks to CEO every Tuesday and Thursday",
+    ],
+  },
+];
